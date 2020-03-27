699b27a2986ed92e13f5eeee031892f5480da040
wll <weileilei@baijiahulian.com>
2020-03-27 20:30:20 +0800

    添加spring-cloud(feign nacos...)

--- src/main/java/com/start/mb/boot/BootApplication.java
+++ src/main/java/com/start/mb/boot/BootApplication.java
@@ -2,10 +2,18 @@ package com.start.mb.boot;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@SpringBootConfiguration
+//@EnableAutoConfiguration
+//@ComponentScan
 @MapperScan("com.start.mb.boot.persistence.mapper")
+@EnableDiscoveryClient
 public class BootApplication {
 
     public static void main(String[] args) {
